903e104e5b7d09066b34e334b926b41df09ceb31
Abner Joia <abnerjoia1991@gmail.com>
2021-01-08 18:38:40 -0300

    Config AutoMapper

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using src.Api.CrossCutting.DependencyInjection;
+using src.Api.CrossCutting.Mappings;
 
 namespace Api.Application
 {
@@ -28,6 +30,15 @@ namespace Api.Application
         {
             services.AddControllers();
             ConfigureRepository.ConfigureDependenciesRepository(services);
+
+            //AutoMapper
+            var config = new AutoMapper.MapperConfiguration(cfg => {
+                cfg.AddProfile(new DtoToModelProfile());
+                cfg.AddProfile(new EntityToDtoProfile());
+                cfg.AddProfile(new ModelToEntityProfile());
+            });
+            IMapper mapper = config.CreateMapper();
+            services.AddSingleton(mapper);            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
