b81a830efced6a9bed0a75430819efc75e93c50c
Abner Joia <abnerjoia1991@gmail.com>
2021-01-21 12:54:16 -0300

    Inclusão AluguelId em ItemAluguelDtoCreateResult

--- src/Api.Domain/Dtos/ItemAluguel/ItemAluguelDtoCreateResult.cs
+++ src/Api.Domain/Dtos/ItemAluguel/ItemAluguelDtoCreateResult.cs
@@ -6,6 +6,7 @@ namespace src.Api.Domain.Dtos.ItemAluguel
     public class ItemAluguelDtoCreateResult
     {
         public Guid Id { get; set; }
+        public Guid AluguelId { get; set; }
         public DateTime Createat { get; set; }
         public FilmeDtoLocacaoResult Filme {get; set;}
     }
